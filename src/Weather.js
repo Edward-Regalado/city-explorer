0df4b6387988c144b6b2510f3f892dc82ad54f72
Edward Regalado <tregalado30@gmail.com>
2021-05-07 12:29:17 -0700

    fixed weather rendering error

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import WeatherDay from './WeatherDay';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 
-class WeatherDay extends React.Component {
+class Weather extends React.Component {
   render() {
+    let weatherList = this.props.weatherData.map((day, index) => (
+      <WeatherDay key={index} description={day.description} date={day.date} />
+    ))
     return(
-      <ListGroup.Item> {this.props.description}: {this.props.date}</ListGroup.Item>
+      <ListGroup.Item>
+        {weatherList}
+      </ListGroup.Item>
     )
   }
 }
 
-export default WeatherDay;
+export default Weather;
 
 // let weatherList = this.props.weatherData.map((day, index) => (
 // <ListGroup.Item key={index}>{`${day.date}: ${day.description}`}</ListGroup.
